33a54ea571f56c818c365129fa753bbefe428a2e
Francesco Pellegrini <francesco.pellegrini@bitrock.it>
2019-10-31 11:53:04 +0100

    rename DaoConfig into DbConfig

--- src/main/g8/src/main/scala/io/github/francescopellegrini/$name__word,lower$/config/AppConfig.scala
+++ src/main/g8/src/main/scala/io/github/francescopellegrini/$name__word,lower$/config/AppConfig.scala
@@ -3,11 +3,11 @@ package io.github.francescopellegrini.$name;format="word,lower"$.config
 import pureconfig.generic.auto._
 
 final case class AppConfig(
-  dao: DaoConfig
+  db: DbConfig
 )
 
 object AppConfig {
 
-  def load: AppConfig = pureconfig.loadConfigOrThrow[AppConfig]
+  def apply(): AppConfig = pureconfig.loadConfigOrThrow[AppConfig]
 
 }
--- src/main/g8/src/main/scala/io/github/francescopellegrini/$name__word,lower$/config/DbConfig.scala
+++ src/main/g8/src/main/scala/io/github/francescopellegrini/$name__word,lower$/config/DbConfig.scala
@@ -1,3 +1,3 @@
 package io.github.francescopellegrini.$name;format="word,lower"$.config
 
-final case class DaoConfig()
+final case class DbConfig()
